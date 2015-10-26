5300e9593d124c2f56c8f9013151c05d0b1b6517
Russell Hueske <hueske.russ690@gmail.com>
2015-10-26 11:34:37 -0500

    Modified Pair.java

--- src/me/ninja4826/bwsal/util/Pair.java
+++ src/me/ninja4826/bwsal/util/Pair.java
@@ -1,27 +1,54 @@
 package me.ninja4826.bwsal.util;
 
-public class Pair<T1, T2> {
+public class Pair<K, V> {
 	
-	public T1 first;
-	public T2 second;
+	public K first;
+	public V second;
 	
 	public Pair() {
-		first = null;
-		second = null;
 	}
 	
-	public Pair(T1 firstVal, T2 secondVal) {
+	public Pair(K firstVal, V secondVal) {
 		this.first = firstVal;
 		this.second = secondVal;
 	}
 	
-	public Pair(Pair<T1, T2> pairToCopy) {
+	public Pair(Pair<K, V> pairToCopy) {
 		this.first = pairToCopy.first;
 		this.second = pairToCopy.second;
 	}
 	
-	public void replace(Pair<T1, T2> pairToCopy) {
+	public void replace(Pair<K, V> pairToCopy) {
 		this.first = pairToCopy.first;
 		this.second = pairToCopy.second;
+	}
+	
+	@Override
+	public String toString() {
+		return "Pair{" +
+				"first=" + this.first +
+				", second=" + this.second +
+				"}";
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		
+		@SuppressWarnings("unchecked")
+		Pair<K, V> pair = (Pair<K, V>) o;
+		
+		if (this.first != null ? !this.first.equals(pair.first) : pair.first != null) return false;
+		if (this.second != null ? !this.second.equals(pair.second) : pair.second != null) return false;
+		
+		return true;
+	}
+	
+	@Override
+	public int hashCode() {
+		int result = first != null ? first.hashCode() : 0;
+		result = 31 * result + (second != null ? second.hashCode() : 0);
+		return result;
 	}
 }
